47fdaa09286424d8c8bcb3deb1bfef265ff61178
huanglei <hlei0815@163.com>
2019-04-02 00:36:23 +0800

    [add]  上传代码
    重新创建lintcode仓库，提交代码

---
+++ src/LintCode.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#pragma warning(disable:4018)
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
